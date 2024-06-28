e3860a9d8c1ee00133bdc6dcd09df606c660f70d
chris estiphanian <estiphanianchris@gmail.com>
2024-06-28 10:43:21 +0300

    files uploaded

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,72 +1,82 @@
-import React, { useEffect, useState } from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom';
-import MainPage from './pages/MainPage';
-import SinglePost from './pages/SinglePost';
-import './App.css';
-import axios from 'axios';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Contacts from './pages/Contacts';
-import About from './pages/About';
-import NewPost from './pages/NewPost';
-import EditPost from './pages/EditPost';
-import Register from './components/Register';
-import NormalDashboard from './components/NormalDashboard';
-import BusinessDashboard from './components/BusinessDashboard';
+import React, { useEffect, useState } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import SinglePost from "./pages/SinglePost";
+import "./App.css";
+import axios from "axios";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Contacts from "./pages/Contacts";
+import About from "./pages/About";
+import NewPost from "./pages/NewPost";
+import EditPost from "./pages/EditPost";
+import Register from "./components/Register";
+import NormalDashboard from "./components/NormalDashboard";
+import BusinessDashboard from "./components/BusinessDashboard";
+import Profile from "./pages/profile";
+import ProfileView from "./pages/profileview"; // Ensure correct capitalization
 
 function App() {
-    const [userType, setUserType] = useState(null);
-    const [loading, setLoading] = useState(true);
+  const [userType, setUserType] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const fetchUserType = async () => {
-            // Assuming you store token in localStorage after login
-            const token = localStorage.getItem('token');
-            if (token) {
-                const config = {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'x-auth-token': token
-                    }
-                };
-
-                try {
-                    const res = await axios.get('/api/auth/user', config);
-                    setUserType(res.data.userType);
-                } catch (err) {
-                    console.error(err);
-                }
-            }
-            setLoading(false);
+  useEffect(() => {
+    const fetchUserType = async () => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        const config = {
+          headers: {
+            "Content-Type": "application/json",
+            "x-auth-token": token,
+          },
         };
 
-        fetchUserType();
-    }, []);
+        try {
+          const res = await axios.get("/api/auth/user", config);
+          setUserType(res.data.userType);
+        } catch (err) {
+          console.error(err);
+        }
+      }
+      setLoading(false);
+    };
 
-    if (loading) {
-        return <div>Loading...</div>;
-    }
+    fetchUserType();
+  }, []);
 
-    return (
-        <div className="App">
-            <Header />
-            <div className="main">
-                <Routes>
-                    <Route path="/register" element={<Register />} />
-                    {userType === 'normal' && <Route path="/dashboard" element={<NormalDashboard />} />}
-                    {userType === 'business' && <Route path="/dashboard" element={<BusinessDashboard />} />}
-                    <Route path="/" element={<Navigate to={userType ? "/dashboard" : "/register"} />} />
-                    <Route path="/post/:id" element={<SinglePost />} />
-                    <Route path="/" element={<MainPage />} />
-                    <Route path="/newpost" element={<NewPost />} />
-                    <Route path="/editpost/:id" element={<EditPost />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/contact" element={<Contacts />} />
-                </Routes>
-            </div>
-            <Footer />
-        </div>
-    );
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="App">
+      <Header />
+      <div className="main">
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          {userType === "normal" && (
+            <Route path="/dashboard" element={<NormalDashboard />} />
+          )}
+          {userType === "business" && (
+            <Route path="/dashboard" element={<BusinessDashboard />} />
+          )}
+          <Route
+            path="/"
+            element={<Navigate to={userType ? "/dashboard" : "/register"} />}
+          />
+          <Route path="/post/:id" element={<SinglePost />} />
+          <Route path="/" element={<MainPage />} />
+          <Route path="/newpost" element={<NewPost />} />
+          <Route path="/editpost/:id" element={<EditPost />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contacts />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/profileview" element={<ProfileView />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
